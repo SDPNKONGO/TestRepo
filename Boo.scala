3328da2618604cabe5c6a9635d42ad33d85fb23c
Hugo Nkongo <nkongohugo@gmail.com>
2016-04-07 19:34:20 +0100

    Added new class BooImpl2 and test object

--- Boo.scala
+++ Boo.scala
@@ -5,3 +5,15 @@ trait Boo {
 class BooImpl extends Boo {
 	override def hello = println("hello")
 }
+
+class BooImpl2 extends Boo {
+	override def hello = println("hello again")
+}
+
+object Test extends App {
+	val b1 = new BooImpl
+	val b2 = new BooImpl2
+
+	b1.hello
+	b2.hello
+}
